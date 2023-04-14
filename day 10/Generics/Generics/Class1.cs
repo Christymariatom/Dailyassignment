106d361a52e29e23376a983c4f0476a2b014ab5a
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-04-14 19:14:04 +0530

    Add files via upload

---
+++ day 10/Generics/Generics/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Clscalculator
+
+    {
+        public static bool AreEqual(int value1,int value2) 
+        {
+            return value1 == value2;
+                }
+    }
+}
---
+++ day 10/Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            bool IsEqual = Clscalculator.AreEqual(20, 20);
+            if (IsEqual)
+            {
+                Console.WriteLine("Both are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both are not equal");
+            }
+        }
+    }
+}
