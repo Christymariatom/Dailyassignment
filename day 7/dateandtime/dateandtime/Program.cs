8f0c1d38af5821c4d553499e113e8970e7c0c71e
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-04-14 19:10:16 +0530

    Add files via upload

---
+++ day 7/dateandtime/dateandtime/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateandtime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime();
+            Console.WriteLine(dt);
+            DateTime dt2 = new DateTime(2015, 12, 31);
+            Console.WriteLine(dt2);
+            DateTime dt3 = new DateTime(2015, 12, 31,5,30,40);
+            Console.WriteLine(dt3);
+            DateTime dt4 = new DateTime(2015, 12, 31,5,30,45,DateTimeKind.Utc);
+            Console.WriteLine(dt4);
+            /* throw expection
+             * DateTime dt5 = new DateTime(2045,13,50);
+             * Console.WriteLine(dt5);*/
+
+           
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine(currentDateTime);
+            DateTime todaysDate = DateTime.Today; 
+            Console.WriteLine(todaysDate);
+            DateTime todaysDay = DateTime.Today;    
+            Console.WriteLine(todaysDay);
+            //DateTimeUTC = DateTime.UtcNow;// returns current UTC date and time
+
+            DateTime maxDateTimeValue = DateTime.MaxValue; 
+            Console.WriteLine(maxDateTimeValue);
+            DateTime minDateTimeValue = DateTime.MinValue;
+            Console.WriteLine(minDateTimeValue);
+
+            DateTime dt6 = new DateTime(345768000000000);
+            Console.WriteLine(dt6);
+            Console.WriteLine(DateTime.MinValue.Ticks);
+            Console.WriteLine(DateTime.MaxValue.Ticks);
+
+            DateTime dt7 = new DateTime(2006, 5, 21);
+            TimeSpan ts = new TimeSpan(6 , 16, 17);
+            DateTime newdate = dt7.Add(ts);
+            Console.WriteLine(newdate); 
+                
+
+
+        }
+    }
+}
---
+++ day 7/notes/notes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount :");
+            int amount = int.Parse(Console.ReadLine());
+            int w = amount / 2000;
+            amount = amount % 2000;
+            Console.WriteLine("No of 2000 notes : " + w);
+            int x = amount / 500;
+            amount = amount % 500;
+            Console.WriteLine("No of 500 notes : " + x);
+            int y = amount / 200;
+            amount = amount % 200;
+            Console.WriteLine("No of 200 notes : " + y);
+            int z = amount / 100;
+            amount = amount % 100;
+            Console.WriteLine("No of 100 notes : " + z);
+            int a = amount / 50;
+            amount = amount % 50;
+            Console.WriteLine("No of 50 note : " + a);
+            int b = amount / 10;
+            amount = amount % 10;
+            Console.WriteLine("No of 10 note : " + b);
+            int c = amount / 5;
+            amount = amount % 5;
+            Console.WriteLine("No of 5 notes : " + c);
+            int d = amount / 2;
+            amount = amount % 2;
+            Console.WriteLine("No of 2 notes : " + d);
+            int e = amount / 1;
+            amount = amount % 1;
+            Console.WriteLine("No of 1 notes : " + e);
+            Console.ReadKey();
+        }
+    }
+}
+
