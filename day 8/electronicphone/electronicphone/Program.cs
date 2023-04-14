1042dfd71bfb0159a5ce1fcd5afd1efe14a720b1
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-04-14 19:12:29 +0530

    Add files via upload

---
+++ day 8/electronicphone/electronicphone/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+
+namespace Telephone
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ElectronicPhone phone = new ElectronicPhone();
+            phone.Ring();
+        }
+        public class Telephone
+        {
+            protected string _Phonetype;
+            public string phonetype
+            {
+                get { return _Phonetype; }
+                set { if (_Phonetype != null) _Phonetype = value; }
+            }
+            public void Ring()
+            {
+                Console.WriteLine("Ringing the {0}", _Phonetype);
+
+            }
+            public Telephone()
+            {
+
+            }
+            public Telephone(string phone)
+            {
+                _Phonetype = phone;
+            }
+        }
+        public class ElectronicPhone : Telephone
+        {
+            public ElectronicPhone()
+            {
+                _Phonetype = "Digital";
+
+            }
+        }
+    }
+}
