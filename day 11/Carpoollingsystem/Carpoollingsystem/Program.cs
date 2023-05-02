6ef508f08f258d338922645b932ed5276e27a26a
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-02 17:13:55 +0530

    Add files via upload

---
+++ day 11/Carpoollingsystem/Carpoollingsystem/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace carpoolreq1
+
+{
+
+    internal class Program1
+
+    {
+        static void Main(string[] args)
+
+        {
+            List<Member> ls = new List<Member>();
+            for (int i = 0; i < 2; i++)
+
+            {
+
+                Console.WriteLine($"member{i + 1}");
+                Console.WriteLine("Id:");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("first name:");
+                string firstname = Console.ReadLine();
+                Console.WriteLine("last name:");
+                string lastname = Console.ReadLine();
+                Console.WriteLine("email:");
+                string email = Console.ReadLine();
+                Console.WriteLine("contact number:");
+                string contactnumber = Console.ReadLine();
+                Console.WriteLine("license number:");
+                string licensenumber = Console.ReadLine();
+                Console.WriteLine("license start date:");
+                DateTime licensestart = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("license end date:");
+                DateTime licenseend = DateTime.Parse(Console.ReadLine());
+                Member member = new Member(id, firstname, lastname, email, contactnumber, licensenumber, licensestart, licenseend);
+
+                ls.Add(member);
+
+            }
+            foreach (Member m in ls)
+            {
+                Console.WriteLine(m.ToString());
+                if (ls[0].Equals(ls[1]))
+                {
+                    Console.WriteLine("member 1 is same as member 2");
+                }
+                else
+                {
+                    Console.WriteLine("MEMBER 1 AND MEMBER 2 ARE DIFFERENT");
+                }
+            }
+        }
+    }
+}
+
---
+++ day 11/Carpoollingsystem/Req5/invalidemailexpection.cs
@@ -0,0 +1,312 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Requirement_5
+
+{
+
+    public class InvalidEmailException : Exception
+
+    {
+
+        string email;
+
+        public InvalidEmailException()
+
+        {
+
+        }
+
+        public InvalidEmailException(string email)
+
+        {
+
+            this.email = email;
+
+        }
+
+        public void inform()
+
+        {
+
+            Console.WriteLine("invalid mailid");
+
+        }
+
+    }
+
+}
+
+
+/*
+////////////////////////////////////////////////////////////////
+
+abstract class Car
+
+{
+
+    private long _id;
+
+    private string _name;
+
+    public long Id
+
+    {
+
+        get
+
+        {
+
+            return _id;
+
+        }
+
+        set
+
+        {
+
+            _id = value;
+
+        }
+
+    }
+
+    public string Name
+
+    {
+
+        get
+
+        {
+
+            return _name;
+
+        }
+
+        set
+
+        {
+
+            _name = value;
+
+        }
+
+    }
+
+    public Car()
+
+    {
+
+    }
+
+    public Car(long id, string name)
+
+    {
+
+        _id = Id;
+
+        _name = Name;
+
+    }
+
+    public abstract double CalculateDriveCost(double km);
+
+}
+
+class HatchBack : Car
+
+{
+
+    private bool _powerWindowsEnabled;
+
+    private bool _automaticGear;
+
+    public bool PowerWindowsEnabled
+
+    {
+
+        get
+
+        {
+
+            return _powerWindowsEnabled;
+
+        }
+
+        set
+
+        {
+
+            _powerWindowsEnabled = value;
+
+        }
+
+    }
+
+    public bool AutomaticGear
+
+    {
+
+        get
+
+        {
+
+            return _automaticGear;
+
+        }
+
+        set
+
+        {
+
+            _automaticGear = value;
+
+        }
+
+    }
+
+    public HatchBack()
+
+    {
+
+    }
+
+    public HatchBack(long id, string name, bool powerWindowsEnabled, bool automaticGear) : base(id, name)
+
+    {
+
+        _powerWindowsEnabled = powerWindowsEnabled;
+
+        _automaticGear = automaticGear;
+
+    }
+
+    public override double CalculateDriveCost(double km);
+
+    {
+
+     double costPerKm =
+
+    class Sedan : Car
+
+    {
+
+        private bool _absEnabled;
+
+        private Int32 _bootspace;
+
+        public bool AbsEnabled
+
+        {
+
+            get
+
+            {
+
+                return _absEnabled;
+
+            }
+
+            set
+
+            {
+
+                _absEnabled = value;
+
+            }
+
+        }
+
+        public Int32 Bootspace
+
+        {
+
+            get
+
+            {
+
+                return _bootspace;
+
+            }
+
+            set
+
+            {
+
+                _bootspace = value;
+
+            }
+
+        }
+
+        public Sedan()
+
+        {
+
+        }
+
+        public Sedan(long id, string name, bool absEnabled, int bootspace) : base(id, name)
+
+        {
+
+            _absEnabled = absEnabled;
+
+            _bootspace = bootspace;
+
+        }
+
+    }
+
+    class UtilityCar : Car
+
+    {
+
+        private bool _rearCoolingVents;
+
+        public bool RearCoolingVents
+
+        {
+
+            get
+
+            {
+
+                return _rearCoolingVents;
+
+            }
+
+            set
+
+            {
+
+                _rearCoolingVents = value;
+
+            }
+
+        }
+
+        public UtilityCar()
+
+        {
+
+        }
+
+        public UtilityCar(long id, string name, bool rearCoolingVents) : base(id, name)
+
+        {
+
+            _rearCoolingVents = rearCoolingVents;
+
+        }
+
+    }
+
+}
+*/
