f8ea68bf31144250c6c9fc68a88431b8a7e812f2
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-02 17:28:23 +0530

    Add files via upload

---
+++ day 17/sortedlist/sortedlist/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortedlist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            SortedList s = new SortedList();
+            s.Add("Maharastra ", "Mumbai");
+            s.Add("Karnataka ", "Bangalore");
+            s.Add("Andhra Pradesh", "Hyderabad");
+            s.Add("Tamilnadu ", "chennai");
+            s.Add("Bihar ", "Patna");
+            s.Add("Rajastan ", "Jaipur");
+            s.Add("Orissa ", "Bhubaneshwar");
+            Console.WriteLine("Elements in the SotredList: ");
+            foreach (SortedList item in s)
+            {
+                Console.WriteLine(item.Keys.ToString());
+            }
+        }
+    }
+}
+
+
+  
