803354dda03b7088e25f974587e9d8241efa3aaf
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-02 17:29:47 +0530

    Add files via upload

---
+++ day 18/arrayex/arrayex/Program 1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+
+
+public class Program
+
+{
+
+    public static void subArraySum(int[] arr, int n, int sum)
+
+    {
+
+        for (int i = 0; i < n; i++)
+
+        {
+
+            int currentSum = arr[i];
+
+            if (currentSum == sum)
+
+            {
+
+                Console.WriteLine("Sum found at index" + i);
+
+                return;
+
+            }
+
+            else
+
+            {
+
+                for (int j = i + 1; j < n; j++)
+
+                {
+
+                    currentSum += arr[j];
+
+                    if (currentSum == sum)
+
+                    {
+
+                        Console.WriteLine("Sum found indexes " + i + " and " + j);
+
+                        return;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        Console.WriteLine("No subarray found");
+
+        return;
+
+    }
+
+    public static void Main(string[] args)
+
+    {
+
+        int[] arr = { 1,2,3,4,5,6,7,8,9,10 };
+
+        int n = arr.Length;
+
+        int sum = 15;
+
+        subArraySum(arr, n, sum);
+
+    }
+
+}
+
+
