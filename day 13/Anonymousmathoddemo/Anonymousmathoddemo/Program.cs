2fe186db6e71f8f3bf1b192d63d3fe606412204e
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-02 17:16:59 +0530

    Add files via upload

---
+++ day 13/Anonymousmathoddemo/Anonymousmathoddemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+delegate void CountIt();
+namespace Anonymousmathoddemo
+{
+    class AnonMetDemo {
+        static void Main()
+        {
+            CountIt count = delegate
+            {
+                for (int i = 0; i < 10; i++)
+                    Console.WriteLine(i);
+            };
+            count();
+        }
+    }   }
+
---
+++ day 13/streamwriter/streamwriter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class WriteAndReader
+{
+    static string file = @"D:\UST DEMO\day 13\streamwriter";
+    static void Main()
+    {
+        Write();
+        Read();
+    }
+    static void Write()
+    {
+        StreamWriter fileStream = new StreamWriter(file);
+        fileStream.WriteLine("WHAT IS THIS FILE IF,FULL OF CARE,");
+        fileStream.WriteLine(" WE HAVE NO TIME TO STAND AND STARE");
+        fileStream.Close();
+    }
+    static void Read()
+    {
+        StreamReader fileStream = new StreamReader(file);
+        string s = null;
+        while ((s = fileStream.ReadLine()) != null)
+        {
+            Console.WriteLine(s);
+        }
+        fileStream.Close();
+    }
+}
