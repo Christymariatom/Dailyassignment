7c461bbe05e01a698576e28fbf84e8b5bb022b4c
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-02 17:15:30 +0530

    Add files via upload

---
+++ day 12/delegates/delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    internal class Program
+    {
+        static void Add (int i,int j)
+        {
+            Console.WriteLine ("SUM"+(i+j));
+        }
+        static void sub(int i, int j)
+        {
+            Console.WriteLine("SUB" + (i - j));
+        }
+        public delegate void reftomethod(int i ,int j);
+        static void Main (string[]args)
+        {
+            /* Add(20, 20);
+             reftomethod r1 = Add;
+                 r1(20,20);
+             reftomethod r2 = sub;
+                 r2(10, 20);*/
+            reftomethod multicast = Add;
+            multicast += sub;
+            multicast(10, 90);
+            Console.WriteLine("After removing sub");
+            multicast -= sub;
+            multicast(10, 90);
+        }
+            
+        }
+    }
+
+
