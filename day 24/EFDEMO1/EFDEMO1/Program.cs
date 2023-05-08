53f1d687e246da1bc7dfae251f504aaba7203c7f
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-08 09:54:08 +0530

    Add files via upload

---
+++ day 24/EFDEMO1/EFDEMO1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDEMO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //insert data
+           /* using (var context = new empdEntities())
+            {
+                var std = new NEWCustomer()
+                {
+                    CustomerID = 6,
+                    CustomerName = "Adhi",
+                    LastName = "KS",
+                    Country = "CHINA",
+                    Age = 23,
+                    Phone = 918858671                 
+                };
+                context.NEWCustomers.Add(std);
+
+                context.SaveChanges();
+            }
+
+            //updating data
+            using (var context = new empdEntities())
+{
+    var std = context.NEWCustomers.First<NEWCustomer>(); 
+    std.CustomerName = "Steve";
+    context.SaveChanges();
+}*/
+
+            
+        }
+    }
+}
