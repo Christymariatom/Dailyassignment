0780f52096b7b9e22504636290f731ac2a98e92e
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-22 23:02:28 +0530

    Add files via upload

---
+++ day 27a/MyFirstCoreWebVST/MyFirstCoreWebVST/Pages/firstpage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstCoreWebVST.Pages
+{
+    public class firstpageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
