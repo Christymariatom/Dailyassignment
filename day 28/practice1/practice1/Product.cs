baf18518008f6d02160b50ccd9a5b5d2cf0396a4
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-22 23:16:23 +0530

    Add files via upload

---
+++ day 28/practice1/practice1/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace practice1
+{
+    public class Product
+    {
+
+        public string _productName;
+
+        public long _id;
+
+        public string _supplierName;
+
+    }
+}
