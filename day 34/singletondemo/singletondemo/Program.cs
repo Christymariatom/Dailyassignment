1c658f58ce7c94cec0fc8af0d0c60cb8d56bb48d
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-22 22:42:26 +0530

    Add files via upload

---
+++ day 34/singletondemo/singletondemo/Program.cs
@@ -0,0 +1,35 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}*/
+using System;
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call the GetInstance static method to get the Singleton Instance
+            Singleton fromTeachaer = Singleton.GetInstance();
+            fromTeachaer.PrintDetails("From Teacher");
+
+            //Call the GetInstance static method to get the Singleton Instance
+            Singleton fromStudent = Singleton.GetInstance();
+            fromStudent.PrintDetails("From Student");
+
+            Console.ReadLine();
+        }
+    }
+}
+
