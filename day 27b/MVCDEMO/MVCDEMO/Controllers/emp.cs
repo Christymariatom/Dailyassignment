dbacc7b01ae18ac5b7a6585f75d191723a6da552
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-22 23:05:11 +0530

    Add files via upload

---
+++ day 27b/MVCDEMO/MVCDEMO/Controllers/emp.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDEMO.Controllers
+{
+    public class emp : Controller
+    {
+        public string Display()
+        {
+            return "HELLO VC Code";
+        }
+        public string Welcome() 
+        {
+            return " hello ";   
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
