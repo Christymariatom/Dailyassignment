176070f6097865556879661ccff846f31c29d8c8
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-29 09:29:10 +0530

    Add files via upload

---
+++ day 40/Array/Array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+     class Program
+    {
+        static void Main(string[] args)
+        
+
+            {
+
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int[] arr = new int[n];
+
+                for (int i = 0; i < arr.Length; i++)
+
+                {
+
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+                foreach (int num in arr)
+
+                {
+
+                    Console.WriteLine(num);
+
+                }
+
+            }
+
+        }
+
+    }
