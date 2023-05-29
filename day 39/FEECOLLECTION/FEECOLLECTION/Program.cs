6a1a782b458d7e27920970f1a5334329efef8b77
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-05-29 09:27:27 +0530

    Add files via upload

---
+++ day 39/FEECOLLECTION/FEECOLLECTION/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEECOLLECTION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Student Type");
+            string studentType = Console.ReadLine();
+            Console.WriteLine("Enter the tution fee");
+            float tutionFee = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the bus fee");
+            float busFee = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the hostel fee");
+            float hostelFee = float.Parse(Console.ReadLine());
+            float TotalFee = CalculateFee(studentType,tutionFee, busFee, hostelFee);
+            Console.WriteLine("The fees need to paid is Rs. " +TotalFee);
+        }
+        static float CalculateFee(string studentType, float tuitionFee, float busFee, float hostelFee)
+
+        {
+            float totalFee = 0;
+            if (studentType == "MSDS")
+            {
+                totalFee = tuitionFee + busFee;
+            }
+
+            else if (studentType == "MSH")
+            {
+                totalFee = tuitionFee + hostelFee;
+            }
+
+            else if (studentType == "MGSDS")
+            {
+                totalFee = 1.5f * tuitionFee + busFee;
+            }
+
+            else if (studentType == "MGSH")
+            {
+                totalFee = 1.5f * tuitionFee + hostelFee;
+            }
+
+            return totalFee;
+
+        }
+
+    }
+
+}
