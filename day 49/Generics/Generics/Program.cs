a75d09542ff3474abb9084ff5186376772141241
Christymariatom <90978185+Christymariatom@users.noreply.github.com>
2023-06-08 17:24:48 +0530

    Add files via upload

---
+++ day 49/Generics/Generics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+
+        {
+
+            int[] intArray = { 1, 2, 3 };
+
+            string[] stringArray = { "Hello", "World" };
+
+            PrintArray(intArray);
+
+            PrintArray(stringArray);
+
+        }
+
+        static void PrintArray<T>(T[] array)
+
+        {
+
+            foreach (T element in array)
+
+            {
+
+                Console.WriteLine(element);
+
+            }
+
+        }
+
+    }
+}
